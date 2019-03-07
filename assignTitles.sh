edb92ba54acb55debf79e1547a5bca19cc502fde
gregoriopellegrino <gregorio.pellegrino@effata.it>
2019-03-07 15:17:45 +0100

    Fixed file paths with spaces

--- assignTitles.sh
+++ assignTitles.sh
@@ -14,13 +14,13 @@
 version="1.0"
 dir_path="$1"
 
-find $dir_path -name '*.xhtml' -or -name '*.html' |
+find "$dir_path" -name '*.xhtml' -or -name '*.html' |
 while read file_path; do
 	
 	echo "$file_path"
 	
 	for level in 1 2 3 4 5 6; do
-		first_heading=$(cat $file_path | pup --charset utf8 "h$level:first-of-type text{}")
+		first_heading=$(cat "$file_path" | pup --charset utf8 "h$level:first-of-type text{}")
 		if [ "$first_heading" != "" ]; then
 			break
 		fi
@@ -29,7 +29,7 @@ while read file_path; do
 	if [ "$first_heading" != "" ]; then
 		first_heading=${first_heading//$'\n'/' '}
 		echo -e "\tFound h$level: $first_heading"
-		sed -i '' -e "s/<title>[^<]*<\/title>/<title>$first_heading<\/title>/g" $file_path
+		sed -i '' -e "s/<title>[^<]*<\/title>/<title>$first_heading<\/title>/g" "$file_path"
 	else
 		echo -e "\t[ERROR] No heading found"
 	fi
